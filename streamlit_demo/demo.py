efceca28fcac6f233073cf4039c2599765a332c0
jekwatt <jekwatt@users.noreply.github.com>
2022-09-01 17:43:21 -0500

    Add demo.py

---
+++ streamlit_demo/demo.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import itertools
+import warnings
+
+import numpy as np
+import pandas as pd
+
+
+# text
+st.title("Welcome to Girls Coding Club")
+st.header("Great to see you here!")
+st.subheader("Hope you are excited about this demo!")
+
+
+# dataframe
+df = pd.DataFrame(
+    data=np.random.randn(10, 4),
+    columns=[f"col_{i}" for i in range(4)]
+)
+
+st.title("DataFrame")
+st.write("Show me the data!")
+st.write(df)
+
+
+# plot
+st.title("Plot")
+
+# basic plot
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4])
+ax.set_xlabel("x-label")
+ax.set_ylabel("y-label")
+ax.set_title('Basic Plot')
+
+st.write(fig)
+
+
+# better plot
+fontsizes = itertools.cycle([16, 16, 24, 32])
+
+
+def example_plot(ax):
+    ax.plot([1, 2])
+    ax.set_xlabel('x-label', fontsize=next(fontsizes))
+    ax.set_ylabel('y-label', fontsize=next(fontsizes))
+    ax.set_title('Better Plot', fontsize=next(fontsizes))
+
+
+fig, ax = plt.subplots(figsize=(8, 5))
+example_plot(ax)
+fig.tight_layout()
+
+st.write(fig)
+
+
+# interaction
+
+# radio
+# multiselect
+st.header("`st.multiselect`")
+options  = st.multiselect(
+    "What is your favorite pet?",
+    ("dog", "cat",  "fish", "bird", "snake")
+)
+st.subheader(f"Multiselect:  **{options}**")
+st.write("---")
+
+# slider
+st.header("`st.slider`")
+value  = st.slider(
+    "Choose a value",
+    min_value=0,
+    max_value=100,
+    step=1,
+    value=50,
+)
+st.subheader(f"Slide selected: {value}")
+st.write("---")
