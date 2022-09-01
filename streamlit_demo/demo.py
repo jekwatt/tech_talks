efe2efc4fac1926146b9e793e99a99c1e48a60f7
jekwatt <jekwatt@users.noreply.github.com>
2022-09-01 17:49:11 -0500

    Run black

--- streamlit_demo/demo.py
+++ streamlit_demo/demo.py
@@ -16,7 +16,7 @@ st.subheader("Hope you are excited about this demo!")
 # dataframe
 df = pd.DataFrame(
     data=np.random.randn(10, 4),
-    columns=[f"col_{i}" for i in range(4)]
+    columns=[f"col_{i}" for i in range(4)],
 )
 
 st.title("DataFrame")
@@ -32,7 +32,7 @@ fig, ax = plt.subplots()
 ax.plot([1, 2, 3, 4])
 ax.set_xlabel("x-label")
 ax.set_ylabel("y-label")
-ax.set_title('Basic Plot')
+ax.set_title("Basic Plot")
 
 st.write(fig)
 
@@ -43,9 +43,9 @@ fontsizes = itertools.cycle([16, 16, 24, 32])
 
 def example_plot(ax):
     ax.plot([1, 2])
-    ax.set_xlabel('x-label', fontsize=next(fontsizes))
-    ax.set_ylabel('y-label', fontsize=next(fontsizes))
-    ax.set_title('Better Plot', fontsize=next(fontsizes))
+    ax.set_xlabel("x-label", fontsize=next(fontsizes))
+    ax.set_ylabel("y-label", fontsize=next(fontsizes))
+    ax.set_title("Better Plot", fontsize=next(fontsizes))
 
 
 fig, ax = plt.subplots(figsize=(8, 5))
@@ -60,16 +60,16 @@ st.write(fig)
 # radio
 # multiselect
 st.header("`st.multiselect`")
-options  = st.multiselect(
+options = st.multiselect(
     "What is your favorite pet?",
-    ("dog", "cat",  "fish", "bird", "snake")
+    ("dog", "cat", "fish", "bird", "snake")
 )
 st.subheader(f"Multiselect:  **{options}**")
 st.write("---")
 
 # slider
 st.header("`st.slider`")
-value  = st.slider(
+value = st.slider(
     "Choose a value",
     min_value=0,
     max_value=100,
