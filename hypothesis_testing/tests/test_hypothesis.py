9e01ce61589a173f75aad769745ecdc9a7689892
jekwatt <jekwatt@users.noreply.github.com>
2024-02-15 13:13:58 -0600

    Rename file, add comments

--- hypothesis_testing/tests/test_hypothesis.py
+++ hypothesis_testing/tests/test_hypothesis.py
@@ -4,8 +4,9 @@ from hypothesis import given, strategies as st
 from hypothesis.strategies import lists, integers, floats, text
 
 
+# Defines lists with different types of elements using hypothesis.strategies.lists
 @given(lists(elements=integers()) | lists(elements=floats()) | lists(elements=text()))
-def test_sorted_property_based(arg):
+def test_sorted_property_based_a(arg):
     result = sorted(arg)
     assert result == sorted(result)
 
