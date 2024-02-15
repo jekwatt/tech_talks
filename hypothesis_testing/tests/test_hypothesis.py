45d95c8a996cedfea2e1fb5f6d7e19bbd7edaeec
jekwatt <jekwatt@users.noreply.github.com>
2024-02-14 20:21:04 -0600

    Add test_hypothesis.py

---
+++ hypothesis_testing/tests/test_hypothesis.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+import pytest
+
+from hypothesis import given
+from hypothesis.strategies import lists, integers, floats, text
+
+
+@given(lists(elements=integers()) | lists(elements=floats()) | lists(elements=text()))
+def test_sorted_property_based(arg):
+    result = sorted(arg)
+    assert result == sorted(result)
