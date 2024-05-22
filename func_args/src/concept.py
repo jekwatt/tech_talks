9b14ae2b9da9c9341aa92f3a431460b0f6dd10e0
jekwatt <jekwatt@users.noreply.github.com>
2024-05-22 08:12:48 -0500

    Run black

--- func_args/src/concept.py
+++ func_args/src/concept.py
@@ -1,5 +1,6 @@
 # Positional arguments (Arguments without default values)
 
+
 def add_a(arg1, arg2):
     return arg1 + arg2
 
@@ -14,6 +15,7 @@ print(add_a(1, 2))
 
 # Keyword arguments
 
+
 # All positional arguments to Python functions can also be passed by keyword
 def add_b(arg1, arg2):
     return arg1 + arg2
@@ -26,11 +28,13 @@ print(add_b(arg2=3, arg1=4))
 def add_c(arg1, arg2=10):
     return arg1 + arg2
 
+
 # Explicitly provide 11 as the value, which overrides the default value of 10
 print(add_c(5, 11))
 
 
 # Keyword-only arguments; after splat (*) in the function signature
+
 
 def add_d(arg1, *, kwarg1, kwarg2):
     return arg1 + kwarg1 + kwarg2
@@ -46,5 +50,5 @@ def add_e(arg1, *, kwarg1, kwarg2=20):  # set default value
     return arg1 + kwarg1 + kwarg2
 
 
-print(add_e(1, kwarg1=3)) # with default value of kwarg2
+print(add_e(1, kwarg1=3))  # with default value of kwarg2
 print(f"add_e: {add_e(5, kwarg1=10, kwarg2=15)}")  # overwide default value of kwarg2
--- func_args/src/fib.py
+++ func_args/src/fib.py
@@ -1,9 +1,11 @@
 # classic fibonacci examples
 
+
 def fib_a(n):
     if n <= 1:
         return n
-    return fib_a(n-1) + fib_a(n-2)
+    return fib_a(n - 1) + fib_a(n - 2)
+
 
 result_a = fib_a(10)
 print(f"fib_a {result_a}")
@@ -11,12 +13,14 @@ print(f"fib_a {result_a}")
 
 # splat(*) followed by keyword-only args
 
+
 def fib_b(n, *, memo={}):  # provide an empty memoization cache
     if n <= 1:
         return n
     elif n not in memo:
-        memo[n] = fib_b(n-1, memo=memo) + fib_b(n-2, memo=memo)
+        memo[n] = fib_b(n - 1, memo=memo) + fib_b(n - 2, memo=memo)
     return memo[n]
+
 
 result_b1 = fib_b(10, memo={})  # Providing the memo argument explicitly
 print(f"Result {result_b1}")
